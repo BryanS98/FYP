b34f916721ac7cb83f8e5401a86ba424165e376a
BryanS98 <72861417+BryanS98@users.noreply.github.com>
2022-02-08 11:56:24 +0000

    Added files via upload
    Initial upload

---
+++ FYP/src/test.java
@@ -0,0 +1,27 @@
+/*
+public class test {
+    
+    public static void main(String args[]){
+        int height = 7;
+        int width = 6;
+        char[][] Ar = new char[height][width];
+        for(int i =0;i<7;i++){
+            for(int j=0;j<6;j++){
+                Ar[i][j] = '.';
+            }
+        }
+
+        printAr(Ar, height, width);
+    }
+}
+
+    ppublic printAr(board[][], int h, int w){
+        for(int i = 0;i < h; i++){
+            for(int j = 0;j < w; w++){
+                System.out.print(board[h][w] + " | ");
+            }
+            System.out.println("\n" + "---------------------------");
+        }
+    }
+}
+*/
