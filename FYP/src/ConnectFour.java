a93b37ac53bde5d33b89a8693ad15d4d9568ef08
BryanS98 <72861417+BryanS98@users.noreply.github.com>
2022-02-09 16:18:36 +0000

    Added Win Check function
    Not yet working but baseline is there

--- FYP/src/ConnectFour.java
+++ FYP/src/ConnectFour.java
@@ -4,55 +4,63 @@ import java.util.*;
 
 public class ConnectFour {
 
+    static Scanner scan = new Scanner(System.in);
+    static int lastCol, lastRow;
     public static void main(String args[]){
-        Connect4();
+        Connect4(createBoard(),1);
     }
 
-    public static void Connect4(){
+    public static int[][] createBoard(){
         int width = 7;
         int height = 6;
-        int Player1 = 1;
-        int Player2 = 2;
         int[][] board = new int[height][width];
 
-        for(int i=0; i < height; i++){
-            for(int j=0; j < width; j++){
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 board[i][j] = 0;
             }
         }
-        printBoard(board, height, width);
-        makeMove(board, Player1);
-        printBoard(board, height, width);
+        return board;
+    }
+
+    public static void Connect4(int[][] board,int Player){
+        printBoard(board);
+        makeMove(board, Player);
+        checkWin(board, Player);
+        nextMove(board,Player);
     }
 
     public static int[][] makeMove(int[][] board, int Player){
         System.out.println("Select column 0-6");
-        Scanner scan = new Scanner(System.in);
         int chosenColumn = scan.nextInt();
-        scan.close();
+        int lastCol = chosenColumn;
+        if(chosenColumn > 6){
+            System.out.println("Invalid Column");
+            chosenColumn=0;
+            makeMove(board, Player);
+        }
         int columnHeight = board.length-1;
+        System.out.println("Column height value " + columnHeight);
         for(int i=0; i<=columnHeight; i++){
-            System.out.println(i);
             if(board[0][chosenColumn]!=0){
                 System.out.println("Column Full, Please choose another Column");
                 return makeMove(board, Player);
             }
             else if(board[i][chosenColumn]!=0){
-                System.out.println("Regular");
                 board[i-1][chosenColumn] = Player;
-                return board;
-            }
-            else {
-                System.out.println("Bottom");
-                board[columnHeight][chosenColumn] = Player;
+                lastRow = i-1;
                 return board;
             }
         }
-
+        board[columnHeight][chosenColumn] = Player;
+        lastRow = columnHeight;
         return board;
+        
     }
 
-    public static void printBoard(int[][] board, int h, int w){
+    public static void printBoard(int[][] board){
+        int h = board.length;
+        int w = board[0].length;
         for(int i = 0;i < h; i++){
             for(int j = 0;j < w; j++){
                 if(j == w-1){
@@ -66,4 +74,91 @@ public class ConnectFour {
             System.out.println("\n" + "---------------------------");
         }
     }
+
+    public static void nextMove(int[][] board, int Player){
+        if(Player == 1){
+            Connect4(board,2);
+        }
+        Connect4(board,1);
+    }
+
+     
+    public static boolean checkWin(int [][] board, int Player){
+        //LastCol and LastRow
+        int lastCounter = board[lastRow][lastCol];
+        int height = board.length;
+        int width = board[0].length;
+        int score = 0;
+        //Horizontal Check
+        int i = 0;
+        while(board[lastCol][lastRow+i]==Player){
+            score++;
+            i++;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("Horizontal win");
+                return true;
+            }
+        }
+        while(board[lastCol][lastRow-i]==Player){
+            score++;
+            i--;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("You Win!");
+                return true;
+            }
+        }
+        
+        //reset score and i value
+        i = 0;
+        score = 0;
+        //Vertical Check
+        while(board[lastCol+i][lastRow]==Player){
+            score++;
+            i++;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("You Win!");
+                return true;
+            }
+        }
+        while(board[lastCol-i][lastRow]==Player){
+            score++;
+            i--;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("You Win!");
+                return true;
+            }
+        }
+
+        //Reset score and i value
+        i = 0;
+        score = 0;
+        //Diagonal Check
+        while(board[lastCol+i][lastRow+i]==Player){
+            score++;
+            i++;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("You Win!");
+                return true;
+            }
+        }
+        while(board[lastCol-i][lastRow-i]==Player){
+            score++;
+            i--;
+            System.out.println("score: " + score);
+            if(score==4) {
+                System.out.println("You Win!");
+                return true;
+            }
+        }
+
+        score = 0;
+        
+        return false;
+    }
+
 }
